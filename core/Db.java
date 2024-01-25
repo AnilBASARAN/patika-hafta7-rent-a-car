afb05096305322f270f5085362e5af5df5edfd8a
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2024-01-25 11:21:11 +0300

    hey silindi
    hey hey

--- core/Db.java
+++ core/Db.java
@@ -33,7 +33,7 @@ public class Db {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-        System.out.println("hey");
+
         return instance.getConnection();
 
     }
